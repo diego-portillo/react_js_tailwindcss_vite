a1b8f993fc4510f5f28813a3e2156a57f0b93db9
Diego <dportillodeveloper@gmail.com>
2023-06-27 12:05:08 -0400

    Clase 17

--- react_vite_tailwind/src/Context/index.jsx
+++ react_vite_tailwind/src/Context/index.jsx
@@ -3,12 +3,38 @@ import {createContext, useState} from 'react'
 export const ShoppingCartContext = createContext()
 
 export const ShoppingCartProvider = ({children})=>{
+    //Shopping Cart count
     const [count, setCount] = useState(0)
-    console.log('Count: ', count)
+    
+    //Open/Close Detail
+    const [isProductDetailOpen, setIsProductDetailOpen] = useState(false)
+    const openProductDetail = () => setIsProductDetailOpen(true)
+    const closeProductDetail = () => setIsProductDetailOpen(false)
+    
+    //Open/Close Checkout
+    const [isCheckoutSideMenuOpen, setIsCheckoutSideMenuOpen] = useState(false)
+    const openCheckoutSideMenu = () => setIsCheckoutSideMenuOpen(true)
+    const closeCheckoutSideMenu = () => setIsCheckoutSideMenuOpen(false)
+
+    // Show Product
+    const [productToShow, setProductToShow] = useState({})
+
+    //Add Products to Cart
+    const [cartProducts, setCartProducts] = useState([])
     return(
         <ShoppingCartContext.Provider value={{
             count,
-            setCount
+            setCount,
+            openProductDetail,
+            closeProductDetail,
+            isProductDetailOpen,
+            productToShow,
+            setProductToShow,
+            cartProducts,
+            setCartProducts,
+            isCheckoutSideMenuOpen,
+            openCheckoutSideMenu,
+            closeCheckoutSideMenu
         }}>
             {children}
         </ShoppingCartContext.Provider>
